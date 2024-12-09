9f0ce102ad558a8783c0eb19500e4b9e296e7bac
juanda1612 <juan.perez77@educa.madrid.com>
2024-12-09 17:10:56 +0100

    Initial commit

---
+++ src/main/java/com/example/pruebaSH/PruebaShApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebaSH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaShApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaShApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebaSH/PruebaShApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebaSH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaShApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
